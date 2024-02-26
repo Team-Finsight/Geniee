542ef0ac90255163d3ebc583ccac9a7703633f1f
Team-Finsight <135005281+Team-Finsight@users.noreply.github.com>
2024-02-26 11:07:11 +0530

    Update general_chat.py

--- general_chat.py
+++ general_chat.py
@@ -3,20 +3,23 @@ from flask import Flask, request, jsonify
 from PIL import Image
 # LangChain Imports
 from langchain import PromptTemplate
-from langchain.chains import ConversationalRetrievalChain, RetrievalQA, LLMChain
+from langchain.chains import ConversationalRetrievalChain, RetrievalQA
 from langchain.embeddings import OpenAIEmbeddings, HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
 from langchain.vectorstores import Chroma, FAISS
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate 
+#from langchain.memory import ConversationBufferWindowMemory
 from langchain.memory import ConversationBufferMemory
-from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader, DirectoryLoader
+#from langchain.document_loaders import PyPDFLoader, TextLoader, Docx2txtLoader, DirectoryLoader
 from langchain.chat_models import ChatAnyscale
-from langchain.agents import ConversationalChatAgent, Tool, AgentExecutor
+#from langchain.agents import ConversationalChatAgent, Tool, AgentExecutor
 from langchain.chains.qa_with_sources import load_qa_with_sources_chain
-from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
+#from langchain.prompts import SystemMessagePromptTemplate, ChatPromptTemplate, HumanMessagePromptTemplate
 # Flask Extensions
 from flask_cors import CORS
-import fitz
-import logging
+#import fitz
+#import logging
 from pythonjsonlogger import jsonlogger
 
 api_key = 'esecret_sn18dj7defst1n3aacssvnc94m'
@@ -26,7 +29,6 @@ def chat(query):
     if not query:
         return jsonify({'error': 'No query provided'})
     try:
-        from langchain import PromptTemplate,  LLMChain
         template = "Chat about {text}"
         prompt = PromptTemplate(template=template, input_variables=["text"])
         llm = ChatAnyscale(anyscale_api_key=api_key, model_name="mistralai/Mixtral-8x7B-Instruct-v0.1")
